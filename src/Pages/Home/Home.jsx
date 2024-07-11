277a63f72a51ee11810fe8c9660d83be401f01d5
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-07-11 16:27:32 +0600

    added job by category section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import Banner from "../../Components/Banner/Banner";
+import JobByCategory from "../../Components/JobByCategory/JobByCategory";
 
 const Home = () => {
       return (
             <div>
                   <Banner />
+
+                  {/* job by category */}
+                  <div className="bg-[#F5F7FA] ">
+                        <h5 style={{ fontFamily: 'Playpen Sans' }} className="text-6xl pt-20 mb-7 font-medium text-center text-[#244034] dark:text-white">Job Listing</h5>
+                        <JobByCategory />
+                  </div>
             </div>
       );
 };
