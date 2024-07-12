75abde5d1a2dd2c90806d15c6aab055ceaacc2e0
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-07-12 16:55:22 +0600

    complete

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-      baseURL: 'http://localhost:5000/',
+      baseURL: 'https://a11-dev-connect-bd-server.vercel.app/',
       withCredentials: true
 });
 
